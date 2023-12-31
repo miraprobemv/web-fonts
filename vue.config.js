15a65d1b1d7b1e908b116b5a5e4c930507c18339
miraprobemv <miraprobemv@gmail.com>
2023-12-31 12:22:27 +0900

    Test fright for github pages

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
+  publicPath: '/web-font/',
   configureWebpack: {
     module: {
       rules: [
